6db96ef6fa796c1d6058e1e1e3f44dd88137c455
Mario Bruno <mbrunostem@gmail.com>
2021-11-13 18:00:46 -0500

    html and js

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var jumbotronEl = document.querySelector(".jumbotron");
+var currentDayEl = document.querySelector("#current-day");
+currentDayEl.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+
+// TODO:
+// Display Time
+// Timeblock change color
+// save text from Timeblock
+// saves to local storage 
+// persists when page refreshes 
