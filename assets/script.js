dd72635d93e73572c1b5094ba8877b7fa08bcd0e
Mario Bruno <mbrunostem@gmail.com>
2021-11-13 18:15:11 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
-var jumbotronEl = document.querySelector(".jumbotron");
-var currentDayEl = document.querySelector("#current-day");
-currentDayEl.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
-
+// var jumbotronEl = document.querySelector(".jumbotron");
+// var currentDayEl = document.querySelector("#current-day");
+// currentDayEl.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+// currentDayEl.textContent = "Hello";
+// var containerEl = document.querySelector(".container");
+// var saveBtnEl = document.querySelector(".saveBtn");
+var day = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#current-day").text(day);
 
 
 // TODO:
